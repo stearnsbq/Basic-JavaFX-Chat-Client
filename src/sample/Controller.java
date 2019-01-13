34bef3b3aa0e4384838b4231f59c667801e7fef5
stearnsbq <stearnsbq@vcu.edu>
2019-01-13 17:46:33 -0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.awt.*;
+import java.net.URI;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+    @FXML
+    Label label;
+    @FXML
+    WebView webView;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public void showResolution(){
+
+
+
+    }
+
+
+
+}
