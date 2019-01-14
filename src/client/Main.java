0fb8c598fc48a9d3a48571e172e73d374b2cf166
stearnsbq <35851288+stearnsbq@users.noreply.github.com>
2019-01-14 02:19:36 -0500

    Update Main.java

--- src/client/Main.java
+++ src/client/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("BIG NIGGA LOGIN");
+        primaryStage.setTitle("LOGIN");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 550, 50));
         primaryStage.show();
