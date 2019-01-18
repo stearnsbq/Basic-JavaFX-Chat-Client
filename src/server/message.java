8fede378204c343f411290dea573b9a9602ce4e8
stearnsbq <35851288+stearnsbq@users.noreply.github.com>
2019-01-18 15:41:45 -0500

    Update message.java

--- src/server/message.java
+++ src/server/message.java
@@ -1,12 +1,31 @@
 package server;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class message implements Serializable {
 
     private String name;
     private String msg;
+    private ArrayList<user> users;
+    private ArrayList<user> list;
 
+    public ArrayList<user> getUsers() {
+        return users;
+    }
+
+    public void setList(HashMap<String, user> hashMap) {
+            this.list = new ArrayList<>(hashMap.values());
+    }
+
+    public ArrayList<user> getList() {
+        return list;
+    }
+
+    public void setUsers(ArrayList<user> users) {
+        this.users = users;
+    }
 
     public String getMsg() {
         return msg;
